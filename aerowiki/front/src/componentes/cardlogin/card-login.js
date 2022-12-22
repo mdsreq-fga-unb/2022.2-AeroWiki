7cc1a632dd70d38d86b809d5bc38c7a61d25f6f7
Miguel <miguel.oliveira@iti.gov.br>
2022-12-22 12:06:00 -0300

    atualização na tela de login

--- aerowiki/front/src/componentes/cardlogin/card-login.js
+++ aerowiki/front/src/componentes/cardlogin/card-login.js
@@ -1,26 +1,40 @@
 import Button from 'react-bootstrap/Button';
 import Form from 'react-bootstrap/Form';
-import Stack from 'react-bootstrap/Stack';
 import Logovert from '../logo-vertical/logovertical';
 import './card-login.css'
 
 function Cardlogin() {
     return (
         <>
-            <div className='container'>
-                <h1 className="Tittle justify-content-center" ><bold>AERO WIKI</bold></h1>
-                <div className="">
-                    <Form>
-                        <Form.Control type="email" placeholder="Matricula" />
-                        <Form.Control type="password" placeholder="Password" />
-                    </Form>
-                    <Button variant="secondary">Entrar</Button>{' '}
+            <h1 className="Title justify-content-center" ><bold>AERO WIKI</bold></h1>
+            <div className="justify-content-center align-items-center">
+                <h3 className="Sub-title">login</h3>
+                <div className='row'>
+                    <div className='col-4' />
+                    <div className='col-4'>
+                        <div className='row'>
+                            <div className='col-2' />
+                            <div className='col-7 menu-login'>
+                                <Form>
+                                    <Form.Control className='username' type="email" placeholder="Matricula" />
+                                    <Form.Control className='senha' type="password" placeholder="Senha" />
+                                </Form>
+                                <div className='row'>
+                                    <div className='col-3' />
+                                    <Button className='secondary' variant="secondary col-5">Entrar</Button>{' '}
+                                </div>
+                            </div>
+                        </div>
+                    </div>
                 </div>
-
-                <Logovert className="align-self-center" />
             </div>
+            <div className='row justify-content-center'>
+                {/* <div className='coluna col-3' /> */}
+                <div className="col-4 logo">
+                    <Logovert/>
+                </div>
                 
-            
+            </div>
         </>
     );
 }
