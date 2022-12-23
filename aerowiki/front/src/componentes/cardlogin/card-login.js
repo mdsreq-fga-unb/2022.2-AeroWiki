c5f1156d0950dedb9092f6e4c7b477a853be0fc8
Miguel <miguelmsoliveira@gmail.com>
2022-12-23 14:14:35 -0300

    refatoração do codigo

--- aerowiki/front/src/componentes/cardlogin/card-login.js
+++ aerowiki/front/src/componentes/cardlogin/card-login.js
@@ -1,46 +1,39 @@
 import Button from 'react-bootstrap/Button';
 import Form from 'react-bootstrap/Form';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
 import FloatingLabel from 'react-bootstrap/FloatingLabel';
 import Logovert from '../logo-vertical/logovertical';
 import './card-login.css'
 
 function Cardlogin() {
+
     return (
         <>
             <h1 className="Title justify-content-center" ><bold>AERO WIKI</bold></h1>
-            <div className="justify-content-center align-items-center">
-                <h3 className="Sub-title">login</h3>
-                <div className='row'>
-                    <div className='col-4' />
-                    <div className='col-4'>
-                        <div className='row'>
-                            <div className='col-2' />
-                            <div className='col-7 menu-login'>
-                                <Form>
-                                    <FloatingLabel label='Matrícula'>
-                                        <Form.Control className='username' type="email" placeholder="Matrícula" />
-                                    </FloatingLabel>
-                                    <FloatingLabel label='Senha'>
-                                        <Form.Control className='senha' type="password" placeholder="Senha" />
-                                    </FloatingLabel>
-                                    <div className='row'>
-                                        <div className='col-3' />
-                                        <Button className='secondary' variant="outline-light col-5">Entrar</Button>{' '}
-                                    </div>
-                                </Form>
-
-                            </div>
-                        </div>
-                    </div>
-                </div>
-            </div>
-            <div className='row justify-content-center'>
-                {/* <div className='coluna col-3' /> */}
-                <div className="col-4 logo">
-                    <Logovert />
-                </div>
-
-            </div>
+            <h3 className="Sub-title justify-content-center">login</h3>
+            <Row className='justify-content-center'>
+                <Col xxl={4} className='menu-login'>
+                    <Row className='justify-content-center'>
+                        <Col xxl={8}>
+                            <Form className='3'>
+                                <FloatingLabel label='Matrícula'>
+                                    <Form.Control className='username' type="email" placeholder="Matrícula" />
+                                </FloatingLabel>
+                                <FloatingLabel label='Senha'>
+                                    <Form.Control className='senha' type="password" placeholder="Senha" />
+                                </FloatingLabel>
+                                <Row className='justify-content-center'>
+                                    <Button className='secondary' variant="outline-light col-5">Entrar</Button>{' '}
+                                </Row>
+                            </Form>
+                        </Col>
+                    </Row>
+                </Col>
+            </Row>
+            <Row className='justify-content-center'>
+                <Logovert />
+            </Row>
         </>
     );
 }
--- aerowiki/front/src/componentes/logo-vertical/logovertical.js
+++ aerowiki/front/src/componentes/logo-vertical/logovertical.js
@@ -3,9 +3,7 @@ import imglogo from '../../imagens/logovertical.svg'
 
 function Logovert() {
   return (
-    <div className="Logovertical">
-      <img src={imglogo} alt="logo"></img>
-    </div>
+   <img className='Logovertical' src={imglogo} alt="logo"></img> 
   )
 }
 
