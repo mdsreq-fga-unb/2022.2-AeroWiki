2f9765e770b2225e3516fc1044be35dc3da19880
trindadea <arthur-catrindadeunb@hotmail.com>
2023-01-09 19:58:55 -0300

    Consegui pegar a URI pelo .env
    Já conectei com o nosso DB

--- aerowiki/back/src/main/config/database.ts
+++ aerowiki/back/src/main/config/database.ts
@@ -1,7 +1,12 @@
-import mongoose, { Mongoose } from 'mongoose';
+import mongoose from 'mongoose';
+import dotenv from 'dotenv';
+dotenv.config( {path: '__dirname + ../../env/.env'} )
+
+const Mongo_URI = process.env.DB_URI
+
 class MongoConnection {
   async connect(): Promise<void>{
-     await mongoose.connect('mongodb+srv://admin:admin@aerowiki.lobnenq.mongodb.net')
+     await mongoose.connect(Mongo_URI)
      console.log("database connected")
   }
 }
