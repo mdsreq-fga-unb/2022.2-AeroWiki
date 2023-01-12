6930809907b6cb3c68936cfa69ac68da78de816c
trindadea <arthur-catrindadeunb@hotmail.com>
2023-01-12 11:13:10 -0300

    Desfazendo alteração do botão Editar, Arquivar, Excluir :(

--- aerowiki/front/src/components/MembersTable/MembersTable.js
+++ aerowiki/front/src/components/MembersTable/MembersTable.js
@@ -44,65 +44,7 @@ function MembersTable() {
                                         <span id='member-telephone'>{item.telephone}</span>
                                         <span id='member-role'>{item.role}</span>
                                     </div>
-
-                                        <Link to='#' id='member-buttons' className={item.email} onClick={showButtons}>
-                                            <FontAwesomeIcon icon={faIcons.faPen} />
-                                            <div id={memberButtons ? "delete-file-edit" : "delete-file-edit-active"}>
-                                                <Link to="#" id='file'><FontAwesomeIcon icon={faIcons.faUserMinus} /></Link >
-                                                <Link to="#" id='edit'><FontAwesomeIcon icon={faIcons.faEraser} onClick={showForm} /></Link >
-                                                <Link to="#" id='delete'><FontAwesomeIcon icon={faIcons.faUserXmark} /></Link >
-                                            </div>
-                                        </Link>
-
-                                        <div id={newuserForm ? 'form-bg' : 'form-bg-active'}></div>
-
-                                        <div id={newuserForm ? 'form' : 'form-active'} >
-                                            <div id='form-container'>
-                                                <span>Editar Membro</span>
-                                                <form id='edit-member' >
-
-                                                    <div id='form-box'>
-                                                        <label>Setor</label>
-                                                        <select className='input'>
-                                                            <option value="" disabled selected>Selecione o setor</option>
-                                                            <option value="Comercial">Comercial</option>
-                                                            <option value="Marketing">Marketing</option>
-                                                            <option value="Relações">Relações Externas</option>
-                                                            <option value="Pesquisa & Desenvolvimento">Pesquisa & Desenvolvimento</option>
-                                                            <option value="Operações">Operações</option>
-                                                        </select>
-                                                    </div>
-
-                                                    <div id='form-box'>
-                                                        <label>Cargo</label>
-                                                        <select className='input'>
-                                                            <option value="" disabled selected>Selecione o cargo</option>
-                                                            <option value="Membro">Membro</option>
-                                                            <option value="Gerente">Gerente</option>
-                                                            <option value="Diretor">Diretor</option>
-                                                            <option value="Presidente">Presidente</option>
-                                                        </select>
-                                                    </div>
-
-                                                </form>
-                                                <div id='form-buttons'>
-                                                    <div className='back area'>
-                                                        <Link to='#' className='button' onClick={showForm}>
-                                                            <FontAwesomeIcon icon={faIcons.faChevronLeft} />
-                                                            <span>Voltar</span>
-                                                        </Link>
-                                                    </div>
-
-                                                    <div className='add area'>
-                                                        <div className='button'>
-                                                            <FontAwesomeIcon icon={faIcons.faEraser} />
-                                                            <button type='submit' form='edit-member'>Atualizar</button>
-                                                        </div>
-                                                    </div>
-                                                </div>
-                                            </div>
-                                        </div>
-
+                                    <MembersButton></MembersButton>
                                 </div>
 
                             </>
